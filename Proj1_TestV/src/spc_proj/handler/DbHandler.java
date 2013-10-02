f5d9edc4bf4d78130288f860ca779c87fef83bc0
sjchung78 <1004-58@hanmail.net>
2013-10-03 00:17:32 +0900

    commit

--- Proj1_TestV/src/spc_proj/handler/DbHandler.java
+++ Proj1_TestV/src/spc_proj/handler/DbHandler.java
@@ -1,5 +1,95 @@
 package spc_proj.handler;
 
-public class DbHandler {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
+import weibo4j.util.WeiboConfig;
+
+public class DbHandler {
+	public Connection conn = null;
+	private LogHandler logger = null;
+	
+	public DbHandler() {
+		// TODO Auto-generated constructor stub
+		this.logger = new LogHandler("DbHandler");
+	}
+
+	public void connect() {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			String connection_string = "jdbc:mysql://"+
+						WeiboConfig.getValue("DB_IP") +":" +
+						WeiboConfig.getValue("DB_PORT") +"/weibo?user=" +
+						WeiboConfig.getValue("DB_ID") + "&password=" +
+						WeiboConfig.getValue("DB_PW");
+		    
+			conn = DriverManager.getConnection(connection_string);	
+		} catch (SQLException ex) {
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		catch (Exception e) {
+		    // handle any errors
+		    e.printStackTrace();
+		}
+	}
+	
+	public void select() {
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+		    stmt = conn.createStatement();
+		    rs = stmt.executeQuery("SELECT * FROM user1");
+
+		    // or alternatively, if you don't know ahead of time that
+		    // the query will be a SELECT...
+
+//		    if (stmt.execute("SELECT foo FROM bar")) {
+//		        rs = stmt.getResultSet();
+//		    }
+
+		    // Now do something with the ResultSet ....
+//		    System.out.println(rs.getString(0));
+		    System.out.println(rs.findColumn("id"));
+		    if(rs.first())
+		    	System.out.println(rs.getString(1));
+		    rs.next();
+		    
+		}
+		catch (SQLException ex){
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		finally {
+		    // it is a good idea to release
+		    // resources in a finally{} block
+		    // in reverse-order of their creation
+		    // if they are no-longer needed
+
+		    if (rs != null) {
+		        try {
+		            rs.close();
+		        } catch (SQLException sqlEx) { } // ignore
+
+		        rs = null;
+		    }
+
+		    if (stmt != null) {
+		        try {
+		            stmt.close();
+		        } catch (SQLException sqlEx) { } // ignore
+
+		        stmt = null;
+		    }
+		}
+	}
 }
--- Proj1_TestV/src/spc_proj/main/Proj1Main.java
+++ Proj1_TestV/src/spc_proj/main/Proj1Main.java
@@ -1,15 +1,16 @@
 package spc_proj.main;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
+import spc_proj.handler.DbHandler;
 import spc_proj.handler.LogHandler;
+import weibo4j.Friendships;
 import weibo4j.Oauth;
 import weibo4j.Timeline;
+import weibo4j.Users;
 import weibo4j.model.Status;
 import weibo4j.model.StatusWapper;
+import weibo4j.model.User;
+import weibo4j.model.UserWapper;
 import weibo4j.model.WeiboException;
-import weibo4j.util.BareBonesBrowserLaunch;
 import weibo4j.util.WeiboConfig;
 
 public class Proj1Main {
@@ -20,23 +21,50 @@ public class Proj1Main {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		LogHandler logger = new LogHandler("Proj1Main");
-
-		Oauth oauth = new Oauth();
-		String access_token = WeiboConfig.getValue("access_token");
-		Timeline tm = new Timeline();
-		tm.client.setToken(access_token);
-		try {
-			StatusWapper status = tm.getPublicTimeline();
-			for(Status s : status.getStatuses()){
-				logger.info(s.toString());
-			}
-			System.out.println(status.getNextCursor());
-			System.out.println(status.getPreviousCursor());
-			System.out.println(status.getTotalNumber());
-			System.out.println(status.getHasvisible());
-		} catch (WeiboException e) {
-			e.printStackTrace();
-		}
+//		String access_token = WeiboConfig.getValue("access_token");
+////		String uid = "3643427605";
+//		
+//		String uid = "1694061470";
+//		Users um = new Users();
+//		um.client.setToken(access_token);
+//		try {
+//			User user = um.showUserById(uid);
+//			logger.info(user.toString());
+//		} catch (WeiboException e) {
+//			e.printStackTrace();
+//		}
+		
+//		String screenName = "Free_Wish";
+//		Friendships fm = new Friendships();
+//		fm.client.setToken(access_token);
+//		try {
+//			String[] ids = fm.getFriendsIdsByName(screenName);
+//			for(String s : ids){
+//				logger.info(s);
+//			}
+//		} catch (WeiboException e) {
+//			e.printStackTrace();
+//		}
+		
+//		String access_token = WeiboConfig.getValue("access_token");
+//		Timeline tm = new Timeline();
+//		tm.client.setToken(access_token);
+//		try {
+//			StatusWapper status = tm.getPublicTimeline();
+//			logger.info(""+status.getTotalNumber());
+//			for(Status s : status.getStatuses()){
+//				logger.info(s.toString());
+//			}
+//			System.out.println(status.getNextCursor());
+//			System.out.println(status.getPreviousCursor());
+//			System.out.println(status.getTotalNumber());
+//			logger.info(status.getHasvisible());
+//		} catch (WeiboException e) {
+//			e.printStackTrace();
+//		}
+		DbHandler dh = new DbHandler();
+		dh.connect();
+		dh.select();
 	}
 
 }
