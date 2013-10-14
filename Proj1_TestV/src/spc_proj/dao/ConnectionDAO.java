226ceb8a167aa3d20873b0df83e4254f2c4fd558
jokerfeng2010 <jokerfeng2010@gmail.com>
2013-10-14 17:01:33 -0400

    Store Connections with time stamp.

--- Proj1_TestV/src/spc_proj/dao/ConnectionDAO.java
+++ Proj1_TestV/src/spc_proj/dao/ConnectionDAO.java
@@ -5,9 +5,11 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.Time;
 
 import spc_proj.handler.DbHandler;
 import spc_proj.handler.LogHandler;
+import spc_proj.utils.WeiboTime;
 import spc_proj.wrapper.WeiboConnection;
 import spc_proj.wrapper.WeiboUser;
 
@@ -16,7 +18,8 @@ public class ConnectionDAO {
 	private WeiboConnection wc = null;
 	private LogHandler logger = null;
 	private DbHandler dh = null;
-	private static PrintWriter fileWriter = null;
+	private volatile static PrintWriter fileWriter = null;
+	private volatile static int count = 0;
 	static{
 			try {
 				fileWriter = new PrintWriter(new File("weibo_connection.txt"));
@@ -51,8 +54,12 @@ public class ConnectionDAO {
 			return false;
 		}
 	}
-	public boolean insert(String nameA, String nameB, int relation){
-		fileWriter.println(nameA + "\n" + nameB + "\n" + relation);
+	public  boolean insert(String nameA, String nameB, int relation){
+		fileWriter.println(nameA + "\t" + nameB + "\t" + relation + "\t" + WeiboTime.getTime());
+		if (++count % 20 == 0)
+			fileWriter.flush();
 		return true;
+	}
+	public static void main(String args[]){
 	}
 }
